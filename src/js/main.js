7294932755504b2477097ebd95c4e9ebda634547
Hunter Mann <hunter.mann24@gmail.com>
2024-06-07 17:05:01 -0400

    First commit

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+console.log("Thank you for visiting my site!")
+console.log("Produced and engineered by hunter mann")
+
+console.log("              _-o#&&*\'\'\'\'?d:>b\\_\r\n          _o\/\"`\'\'  \'\',, dMF9MMMMMHo_\r\n       .o&#\'        `\"MbHMMMMMMMMMMMHo.\r\n     .o\"\" \'         vodM*$&&HMMMMMMMMMM?.\r\n    ,\'              $M&ood,~\'`(&##MMMMMMH\\\r\n   \/               ,MMMMMMM#b?#bobMMMMHMMML\r\n  &              ?MMMMMMMMMMMMMMMMM7MMM$R*Hk\r\n ?$.            :MMMMMMMMMMMMMMMMMMM\/HMMM|`*L\r\n|               |MMMMMMMMMMMMMMMMMMMMbMH\'   T,\r\n$H#:            `*MMMMMMMMMMMMMMMMMMMMb#}\'  `?\r\n]MMH#             \"\"*\"\"\"\"*#MMMMMMMMMMMMM\'    -\r\nMMMMMb_                   |MMMMMMMMMMMP\'     :\r\nHMMMMMMMHo                 `MMMMMMMMMT       .\r\n?MMMMMMMMP                  9MMMMMMMM}       -\r\n-?MMMMMMM                  |MMMMMMMMM?,d-    \'\r\n :|MMMMMM-                 `MMMMMMMT .M|.   :\r\n  .9MMM[                    &MMMMM*\' `\'    .\r\n   :9MMk                    `MMM#\"        -\r\n     &M}                     `          .-\r\n      `&.                             .\r\n        `~,   .                     .\/\r\n            . _                  .-\r\n              \'`--._,dd###pp=\"\"\' The World is OUR PLAYGROUND, Create to your hearts desire...")
+
+
+
+
+
+
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./**/*.{html,js}', './src/**/*.{html,js}'],
+    theme: {
+      screens: {
+        'mobile': '375px',
+  
+        'mobile-2': '414px',
+        'samsung': '412px',
+        'small': '320px'
+      },
+      extend: {
+        fontFamily: {
+  
+          'main':['Roboto', 'sans-serif']
+        },
+        colors: {
+          'logo-red': '#D72323',
+        },
+        backgroundImage: {
+          'star-icon': "url('https://img.icons8.com/?size=100&id=qdQpy48X3Rjv&format=png&color=000000')",
+        },
+      },
+    },
+    plugins: [],
+  };
+  
