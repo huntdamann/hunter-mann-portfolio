cb9a31e2e55962085bc7aa84445fce8002b2a7ad
Hunter Mann <hunter.mann24@gmail.com>
2024-06-08 09:02:54 -0400

    adding slight Fade-in transition to homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,28 @@ module.exports = {
       small: "320px",
     },
     extend: {
+
+      animation: {
+
+        fade: 'fadeDown 0.5s ease-in',
+        fadetwo: 'fadeUp 0.5s ease-in'
+      },
+      keyframes: {
+
+        fadeDown: {
+          '0%': { opacity: '0', transform: 'translateY(-30px) scale(0.9)'},
+          '100%': {opacity: '1', transform: 'translateY(0px) scale(1)'}
+
+        },
+        fadeUp: {
+          '0%': { opacity: '0', transform: 'translateY(0px) scale(0.9)'},
+          '100%': {opacity: '1', transform: 'translateY(-30pxpx) scale(1)'}
+
+        }
+      },
+
+
+
       fontFamily: {
         main: ["Roboto", "sans-serif"],
         engineer: ["Kanit", "sans-serif"]
