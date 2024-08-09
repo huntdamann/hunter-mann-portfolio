a743022846e53c2b782d29a0b84362b342f5f478
Hunter Mann <hunter.mann24@gmail.com>
2024-08-09 01:02:18 -0400

    Updating books.html + index.html

--- src/js/observer.js
+++ src/js/observer.js
@@ -1,52 +1,26 @@
 const workSection = document.getElementById('worksection')
 const picture = document.getElementById('pic-container')
 const skill = document.getElementById('skill')
-const options = {
-    root: null,
+
+
+
+window.addEventListener(
+    "load",
+    (event) => {
+      createObserver();
+    },
+    false,
+  );
+
+
+let options = {
+    root: document.querySelector("#skill"),
     rootMargin: "0px",
-    threshold: 1,
+    threshold: 1.0,
+
 };
-const observer = new IntersectionObserver(intersectionCallback, options);
-const skillObserver = new IntersectionObserver(callback, options)
 
-console.log(workSection);
-observer.observe(workSection);
-skillObserver.observe(skill);
-    
-
-
-function intersectionCallback(entries) {
-   
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            console.log("Education section is shown");
-            picture.classList.add('showPicture')
-        }
-        else {
-            console.log("I am currently not in the viewport");
-            picture.classList.remove('showPicture')
-        }
-    })
-}
-
-function callback(entries) {
-    entries.forEach((entry) => {
-        
-        if (entry.isIntersecting) {
-            console.log("Skill is in view")
-        }
-        else {
-            console.log("Skill is not in view anymore")
-        }
-
-
-
-    })
-}
-
-
-
-
+let observer = new IntersectionObserver(callback,options);
 
 
 
